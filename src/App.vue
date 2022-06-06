de81615d902547aac63ca072a4d4cea827316c16
Your Name <you@example.com>
2022-06-06 13:55:55 -0400

    Add mais um projeto

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <map-component></map-component>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MapComponent from './components/MapComponent.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MapComponent
   }
 }
 </script>
---
+++ src/components/MapComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+export default {
+  name: "MapComponent",
+  data: () => ({
+    map: null,
+    tileLayer: null,
+    layers: [],
+  }),
+  mounted() {
+    this.initMap();
+    this.initLayers();
+  },
+  methods: {
+    initMap() {
+      this.map = L.map("map").setView([-25.46033726712044, -49.29804536309487], 12);
+    },
+    initLayer() {
+      this.tileLayer = L.tileLayer(
+        "https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png",
+        {
+          maxZoom: 18,
+          attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+        }
+      );
+      this.tileLayer.addTo(this.map);
+    },
+  },
+};
+</script>
+
+<style></style>
